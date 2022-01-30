47c9cce648b21424a6c8ca598e4fec71e813ab7c
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2022-01-29 21:04:26 -0500

    Add files via upload

--- src/profile_makers/ucs_domain_profile_maker/ucs_domain_profile_maker.py
+++ src/profile_makers/ucs_domain_profile_maker/ucs_domain_profile_maker.py
@@ -85,39 +85,53 @@ import re
 import time
 
 # Function to get Intersight API client as specified in the Intersight Python SDK documentation for OpenAPI 3.x
-def get_api_client(api_key_id, api_secret_file, endpoint="https://intersight.com"):
-    with open(api_secret_file, 'r') as f:
-        api_key = f.read()
+## Modified to align with overall formatting and try/except blocks added for additional error handling
+def get_api_client(api_key_id,
+                   api_secret_file,
+                   endpoint="https://intersight.com"
+                   ):
+    try:
+        with open(api_secret_file, 'r') as f:
+            api_key = f.read()
+        
+        if re.search('BEGIN RSA PRIVATE KEY', api_key):
+            # API Key v2 format
+            signing_algorithm = intersight.signing.ALGORITHM_RSASSA_PKCS1v15
+            signing_scheme = intersight.signing.SCHEME_RSA_SHA256
+            hash_algorithm = intersight.signing.HASH_SHA256
 
-    if re.search('BEGIN RSA PRIVATE KEY', api_key):
-        # API Key v2 format
-        signing_algorithm = intersight.signing.ALGORITHM_RSASSA_PKCS1v15
-        signing_scheme = intersight.signing.SCHEME_RSA_SHA256
-        hash_algorithm = intersight.signing.HASH_SHA256
+        elif re.search('BEGIN EC PRIVATE KEY', api_key):
+            # API Key v3 format
+            signing_algorithm = intersight.signing.ALGORITHM_ECDSA_MODE_DETERMINISTIC_RFC6979
+            signing_scheme = intersight.signing.SCHEME_HS2019
+            hash_algorithm = intersight.signing.HASH_SHA256
 
-    elif re.search('BEGIN EC PRIVATE KEY', api_key):
-        # API Key v3 format
-        signing_algorithm = intersight.signing.ALGORITHM_ECDSA_MODE_DETERMINISTIC_RFC6979
-        signing_scheme = intersight.signing.SCHEME_HS2019
-        hash_algorithm = intersight.signing.HASH_SHA256
-
-    configuration = intersight.Configuration(
-        host=endpoint,
-        signing_info=intersight.signing.HttpSigningConfiguration(
-            key_id=api_key_id,
-            private_key_path=api_secret_file,
-            signing_scheme=signing_scheme,
-            signing_algorithm=signing_algorithm,
-            hash_algorithm=hash_algorithm,
-            signed_headers=[
-                intersight.signing.HEADER_REQUEST_TARGET,
-                intersight.signing.HEADER_HOST,
-                intersight.signing.HEADER_DATE,
-                intersight.signing.HEADER_DIGEST,
-            ]
-        )
-    )
-
+        configuration = intersight.Configuration(
+            host=endpoint,
+            signing_info=intersight.signing.HttpSigningConfiguration(
+                key_id=api_key_id,
+                private_key_path=api_secret_file,
+                signing_scheme=signing_scheme,
+                signing_algorithm=signing_algorithm,
+                hash_algorithm=hash_algorithm,
+                signed_headers=[
+                    intersight.signing.HEADER_REQUEST_TARGET,
+                    intersight.signing.HEADER_HOST,
+                    intersight.signing.HEADER_DATE,
+                    intersight.signing.HEADER_DIGEST,
+                    ]
+                )
+            )
+    except Exception:
+        print("\nA configuration error has occurred!\n")
+        print("Unable to access the Intersight API Key.")
+        print("Exiting due to the Intersight API Key being unavailable.\n")
+        print("Please verify that the correct API Key ID and API Key have "
+              "been entered, then re-attempt execution.\n")
+        print("Exception Message: ")
+        traceback.print_exc()
+        sys.exit(0)
+        
     return intersight.ApiClient(configuration)
 
 
@@ -197,6 +211,7 @@ def test_intersight_api_service(intersight_api_key_id,
         print("Exiting due to the Intersight API being unavailable.\n")
         print("Please verify that the correct API Key ID and API Key have "
               "been entered, then re-attempt execution.\n")
+        print("Exception Message: ")
         traceback.print_exc()
         sys.exit(0)
 
