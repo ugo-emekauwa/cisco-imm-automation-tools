b4a8db0994fe43d7c4269cde2cfcf83b218b8465
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2022-02-02 02:40:15 -0500

    Add files via upload

--- src/profile_makers/ucs_domain_profile_maker/ucs_domain_profile_maker.py
+++ src/profile_makers/ucs_domain_profile_maker/ucs_domain_profile_maker.py
@@ -67,7 +67,7 @@ target_assignment_identifier = ""
 assign_ucs_domain_profile = False
 
 # Intersight Base URL Setting (Change only if using the Intersight Virtual Appliance)
-intersight_base_url = "https://intersight.com/api/v1"
+intersight_base_url = "https://www.intersight.com/api/v1"
 
 ####### Finish Configuration Settings - The required value entries are complete. #######
 
@@ -138,7 +138,7 @@ def get_api_client(api_key_id,
 # Establish function to test for the availability of the Intersight API and Intersight account
 def test_intersight_api_service(intersight_api_key_id,
                                 intersight_api_key,
-                                intersight_base_url="https://intersight.com/api/v1",
+                                intersight_base_url="https://www.intersight.com/api/v1",
                                 preconfigured_api_client=None
                                 ):
     """This is a function to test the availability of the Intersight API and
@@ -152,9 +152,9 @@ def test_intersight_api_service(intersight_api_key_id,
             The system file path of the Intersight API key.
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance. The
-            default value is "https://intersight.com/api/v1".
+            default value is "https://www.intersight.com/api/v1".
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
             Intersight client-server communication through the use of API keys.
@@ -223,7 +223,7 @@ def intersight_object_moid_retriever(intersight_api_key_id,
                                      intersight_api_path,
                                      object_type="object",
                                      organization="default",
-                                     intersight_base_url="https://intersight.com/api/v1",
+                                     intersight_base_url="https://www.intersight.com/api/v1",
                                      preconfigured_api_client=None
                                      ):
     """This is a function to retrieve the MOID of Intersight objects
@@ -246,7 +246,7 @@ def intersight_object_moid_retriever(intersight_api_key_id,
             The default value is "default".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
@@ -369,7 +369,7 @@ def get_intersight_objects(intersight_api_key_id,
                            intersight_api_key,
                            intersight_api_path,
                            object_type="object",
-                           intersight_base_url="https://intersight.com/api/v1",
+                           intersight_base_url="https://www.intersight.com/api/v1",
                            preconfigured_api_client=None
                            ):
     """This is a function to perform an HTTP GET on all objects under an
@@ -391,7 +391,7 @@ def get_intersight_objects(intersight_api_key_id,
             "object".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
@@ -448,7 +448,7 @@ def get_single_intersight_object(intersight_api_key_id,
                                  intersight_api_path,
                                  object_moid,
                                  object_type="object",
-                                 intersight_base_url="https://intersight.com/api/v1",
+                                 intersight_base_url="https://www.intersight.com/api/v1",
                                  preconfigured_api_client=None
                                  ):
     """This is a function to perform an HTTP GET on a single object under an
@@ -472,7 +472,7 @@ def get_single_intersight_object(intersight_api_key_id,
             "object".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
@@ -530,7 +530,7 @@ def advanced_intersight_object_moid_retriever(intersight_api_key_id,
                                               intersight_api_path,
                                               object_type="object",
                                               organization="default",
-                                              intersight_base_url="https://intersight.com/api/v1",
+                                              intersight_base_url="https://www.intersight.com/api/v1",
                                               preconfigured_api_client=None
                                               ):
     """This is a function to retrieve the MOID of Intersight objects based on
@@ -554,7 +554,7 @@ def advanced_intersight_object_moid_retriever(intersight_api_key_id,
             The default value is "default".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
@@ -793,7 +793,7 @@ class UcsDomainProfile:
                  ucs_domain_profile_name,
                  ucs_domain_profile_description="",
                  ucs_domain_profile_organization="default",
-                 intersight_base_url="https://intersight.com/api/v1",
+                 intersight_base_url="https://www.intersight.com/api/v1",
                  tags=None,
                  preconfigured_api_client=None
                  ):
@@ -858,6 +858,51 @@ class UcsDomainProfile:
             print(f"The configuration of the base {self.object_type} "
                   "has completed.")
             return "The POST method was successful."
+        except intersight.exceptions.ApiException as error:
+            if error.status == 409:
+                existing_intersight_object_name = self.intersight_api_body.get("Name", "object")
+                print(f"The targeted {self.object_type} appears to already "
+                      "exist.")
+                print("An attempt will be made to update the pre-existing "
+                      f"{existing_intersight_object_name}...")
+                try:
+                    existing_intersight_object_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
+                                                                                       intersight_api_key=None,
+                                                                                       object_name=existing_intersight_object_name,
+                                                                                       intersight_api_path=self.intersight_api_path,
+                                                                                       object_type=self.object_type,
+                                                                                       preconfigured_api_client=self.api_client
+                                                                                       )
+                    # Update full Intersight API path with the MOID of the existing object
+                    full_intersight_api_path_with_moid = f"/{self.intersight_api_path}/{existing_intersight_object_moid}"
+                    self.api_client.call_api(resource_path=full_intersight_api_path_with_moid,
+                                             method="POST",
+                                             body=self.intersight_api_body,
+                                             auth_settings=['cookieAuth', 'http_signature', 'oAuth2', 'oAuth2']
+                                             )
+                    print(f"The update of the {self.object_type} has "
+                          "completed.")
+                    print(f"The pre-existing {existing_intersight_object_name} "
+                          "has been updated.")
+                    return "The POST method was successful."
+                except Exception:
+                    print("\nA configuration error has occurred!\n")
+                    print(f"Unable to update the {self.object_type} under the "
+                          "Intersight API resource path "
+                          f"'{full_intersight_api_path_with_moid}'.\n")
+                    print(f"The pre-existing {existing_intersight_object_name} "
+                          "could not be updated.")
+                    print("Exception Message: ")
+                    traceback.print_exc()
+                    return "The POST method failed."
+            else:
+                print("\nA configuration error has occurred!\n")
+                print(f"Unable to configure the {self.object_type} under the "
+                      "Intersight API resource path "
+                      f"'{full_intersight_api_path}'.\n")
+                print("Exception Message: ")
+                traceback.print_exc()
+                return "The POST method failed."
         except Exception:
             print("\nA configuration error has occurred!\n")
             print(f"Unable to configure the {self.object_type} under the "
@@ -919,7 +964,7 @@ class SwitchProfile(UcsDomainProfile):
                  ucs_domain_profile_name,
                  ucs_domain_profile_description="",
                  ucs_domain_profile_organization="default",
-                 intersight_base_url="https://intersight.com/api/v1",
+                 intersight_base_url="https://www.intersight.com/api/v1",
                  tags=None,
                  preconfigured_api_client=None,
                  fabric_interconnect="A"
@@ -1075,7 +1120,7 @@ def ucs_domain_profile_maker(intersight_api_key_id,
                              ucs_domain_profile_name,
                              ucs_domain_profile_description="",
                              ucs_domain_profile_organization="default",
-                             intersight_base_url="https://intersight.com/api/v1",
+                             intersight_base_url="https://www.intersight.com/api/v1",
                              tags=None,
                              preconfigured_api_client=None
                              ):
@@ -1096,7 +1141,7 @@ def ucs_domain_profile_maker(intersight_api_key_id,
             Profile. The default value is "default".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         tags (dict):
             Optional; The Intersight account tags that will be assigned to the
@@ -1179,7 +1224,7 @@ def assign_and_deploy_ucs_domain_profile(intersight_api_key_id,
                                          assign_ucs_domain_profile=True,
                                          deploy_ucs_domain_profile=False,
                                          ucs_domain_profile_organization="default",
-                                         intersight_base_url="https://intersight.com/api/v1",
+                                         intersight_base_url="https://www.intersight.com/api/v1",
                                          preconfigured_api_client=None
                                          ):
     """This is a function to assign and deploy a UCS Domain Profile to
@@ -1212,7 +1257,7 @@ def assign_and_deploy_ucs_domain_profile(intersight_api_key_id,
             Profile. The default value is "default".
         intersight_base_url (str):
             Optional; The base URL for Intersight API paths. The default value
-            is "https://intersight.com/api/v1". This value typically only
+            is "https://www.intersight.com/api/v1". This value typically only
             needs to be changed if using the Intersight Virtual Appliance.
         preconfigured_api_client ("ApiClient"):
             Optional; An ApiClient class instance which handles
