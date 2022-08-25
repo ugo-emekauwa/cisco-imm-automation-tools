1a5e24c29bd6601ac2c187f1a61d28467f14cc19
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2022-08-25 18:43:33 -0400

    Update ucs_domain_deployment_tool.py

--- src/deployment_tools/ucs_domain_deployment_tool/ucs_domain_deployment_tool.py
+++ src/deployment_tools/ucs_domain_deployment_tool/ucs_domain_deployment_tool.py
@@ -16,6 +16,7 @@ import copy
 import intersight
 import re
 import time
+import urllib3
 
 ########################
 # MODULE REQUIREMENT 1 #
@@ -64,6 +65,7 @@ values and replace them with your own, where applicable.
 
 # Intersight Base URL Setting (Change only if using the Intersight Virtual Appliance)
 intersight_base_url = "https://www.intersight.com/api/v1"
+url_certificate_verification = True
 
 # Global Deployment Name Settings (Optional - These settings will attach a global prefix or suffix to the names of the created UCS Domain Profile and Policies)
 deployment_name_prefix = ""
@@ -562,11 +564,15 @@ custom_default_link_aggregation_policy_name = ""
 #############################################################################################################################
 
 
+# Suppress InsecureRequestWarning
+urllib3.disable_warnings()
+
 # Function to get Intersight API client as specified in the Intersight Python SDK documentation for OpenAPI 3.x
-## Modified to align with overall formatting and try/except blocks added for additional error handling
+## Modified to align with overall formatting, try/except blocks added for additional error handling, certificate verification option added
 def get_api_client(api_key_id,
                    api_secret_file,
-                   endpoint="https://intersight.com"
+                   endpoint="https://intersight.com",
+                   url_certificate_verification=True
                    ):
     try:
         with open(api_secret_file, 'r') as f:
@@ -600,6 +606,9 @@ def get_api_client(api_key_id,
                     ]
                 )
             )
+
+        if not url_certificate_verification:
+            configuration.verify_ssl = False
     except Exception:
         print("\nA configuration error has occurred!\n")
         print("Unable to access the Intersight API Key.")
@@ -7665,7 +7674,8 @@ def main():
     # Establish Intersight SDK for Python API client instance
     main_intersight_api_client = get_api_client(api_key_id=key_id,
                                                 api_secret_file=key,
-                                                endpoint=intersight_base_url
+                                                endpoint=intersight_base_url,
+                                                url_certificate_verification=url_certificate_verification
                                                 )
     
     # Starting the UCS Domain Deployment Tool for Cisco Intersight
