c39bc4e39904ee9f6b16c9cbcc9267075c143e06
Ugo Emekauwa <51210375+ugo-emekauwa@users.noreply.github.com>
2022-12-07 17:02:35 -0500

    Add files via upload

--- src/policy_makers/serial_over_lan_policy_maker/serial_over_lan_policy_maker.py
+++ src/policy_makers/serial_over_lan_policy_maker/serial_over_lan_policy_maker.py
@@ -64,8 +64,9 @@ ssh_port = 2400        # Options: 1024 - 65535
 intersight_base_url = "https://www.intersight.com/api/v1"
 url_certificate_verification = True
 
-# UCS Server Profile Attachment Settings
-ucs_server_profile_name = ""
+# UCS Server Profile Attachment Settings (If providing more than one UCS Server Profile and/or UCS Server Profile Template, additional entries should be comma-separated)
+ucs_server_profile_names = ""
+ucs_server_profile_template_names = ""
 
 ####### Finish Configuration Settings - The required value entries are complete. #######
 
@@ -329,7 +330,7 @@ def intersight_object_moid_retriever(intersight_api_key_id,
                 provided_organization_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
                                                                               intersight_api_key=None,
                                                                               object_name=organization,
-                                                                              intersight_api_path="organization/Organizations",
+                                                                              intersight_api_path="organization/Organizations?$top=1000",
                                                                               object_type="Organization",
                                                                               preconfigured_api_client=api_client
                                                                               )
@@ -528,6 +529,102 @@ def get_single_intersight_object(intersight_api_key_id,
         sys.exit(0)
 
 
+# Establish function to convert a list of strings in string type format to list type format.
+def string_to_list_maker(string_list,
+                         remove_duplicate_elements_in_list=True
+                         ):
+    """This function converts a list of strings in string type format to list
+    type format. The provided string should contain commas, semicolons, or
+    spaces as the separator between strings. For each string in the list,
+    leading and rear spaces will be removed. Duplicate strings in the list are
+    removed by default.
+
+    Args:
+        string_list (str):
+            A string containing an element or range of elements.
+
+        remove_duplicate_elements_in_list (bool):
+            Optional; A setting to determine whether duplicate elements are
+            removed from the provided string list. The default value is True.
+
+    Returns:
+        A list of elements.   
+    """
+    def string_to_list_separator(string_list,
+                                 separator
+                                 ):
+        """This function converts a list of elements in string type format to
+        list type format using the provided separator. For each element in the
+        list, leading and rear spaces are removed.
+
+        Args:
+            string_list (str):
+                A string containing an element or range of elements.
+
+            separator (str):
+                The character to identify where elements in the
+                list should be separated (e.g., a comma, semicolon,
+                hyphen, etc.).
+
+        Returns:
+            A list of separated elements that have been stripped of any spaces.   
+        """
+        fully_stripped_list = []
+        # Split string by provided separator and create list of separated elements.
+        split_list = string_list.split(separator)
+        for element in split_list:
+            if element:
+                # Remove leading spaces from elements in list.
+                lstripped_element = element.lstrip()
+                # Remove rear spaces from elements in list.
+                rstripped_element = lstripped_element.rstrip()
+                # Populate new list with fully stripped elements.
+                fully_stripped_list.append(rstripped_element)
+        return fully_stripped_list
+
+    def list_to_list_separator(provided_list,
+                               separator
+                               ):
+        """This function converts a list of elements in list type format to
+        list type format using the provided separator. For each element in the
+        list, leading and rear spaces are removed.
+
+        Args:
+            provided_list (list): A list of elements to be separated.
+
+            separator (str): The character to identify where elements in the
+                list should be separated (e.g., a comma, semicolon,
+                hyphen, etc.).
+
+        Returns:
+            A list of separated elements that have been stripped of any spaces.        
+        """
+        new_list = []
+        # Split list by provided separator and create new list of separated elements.
+        for element in provided_list:
+            if separator in element:
+                split_provided_list = string_to_list_separator(element, separator)
+                new_list.extend(split_provided_list)
+            else:
+                new_list.append(element)
+        return new_list
+    
+    staged_list = []
+    # Split provided list by spaces.
+    space_split_list = string_to_list_separator(string_list, " ")
+    # Split provided list by commas.
+    post_comma_split_list = list_to_list_separator(space_split_list, ",")
+    # Split provided list by semicolons.
+    post_semicolon_split_list = list_to_list_separator(post_comma_split_list, ";")
+    # Split provided list by hyphens.
+    for post_semicolon_split_string_set in post_semicolon_split_list:
+        staged_list.append(post_semicolon_split_string_set)
+    # Remove duplicates from list if enabled.
+    if remove_duplicate_elements_in_list:
+        final_list = list(set(staged_list))
+    return final_list
+
+
 # Establish Maker specific classes and functions
 class UcsPolicy:
     """This class is used to configure a UCS Policy in Intersight.
@@ -620,7 +717,7 @@ class UcsPolicy:
                     existing_intersight_object_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
                                                                                        intersight_api_key=None,
                                                                                        object_name=existing_intersight_object_name,
-                                                                                       intersight_api_path=self.intersight_api_path,
+                                                                                       intersight_api_path=f"{self.intersight_api_path}?$top=1000",
                                                                                        object_type=self.object_type,
                                                                                        preconfigured_api_client=self.api_client
                                                                                        )
@@ -671,7 +768,7 @@ class UcsPolicy:
         policy_organization_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
                                                                     intersight_api_key=None,
                                                                     object_name=self.organization,
-                                                                    intersight_api_path="organization/Organizations",
+                                                                    intersight_api_path="organization/Organizations?$top=1000",
                                                                     object_type="Organization",
                                                                     preconfigured_api_client=self.api_client
                                                                     )
@@ -923,8 +1020,8 @@ class UcsPolicy:
 
 class DirectlyAttachedUcsServerPolicy(UcsPolicy):
     """This class is used to configure a UCS Server Policy in Intersight that
-    is logically directly attached to UCS Servers through UCS
-    Server Profiles.
+    is logically directly attached to UCS Servers through UCS Server Profiles
+    and/or UCS Server Profile Templates.
     """
     object_type = "Directly Attached UCS Server Policy"
     intersight_api_path = None
@@ -938,7 +1035,8 @@ class DirectlyAttachedUcsServerPolicy(UcsPolicy):
                  intersight_base_url="https://www.intersight.com/api/v1",
                  tags=None,
                  preconfigured_api_client=None,
-                 ucs_server_profile_name=""
+                 ucs_server_profile_names="",
+                 ucs_server_profile_template_names=""
                  ):
         super().__init__(intersight_api_key_id,
                          intersight_api_key,
@@ -949,7 +1047,8 @@ class DirectlyAttachedUcsServerPolicy(UcsPolicy):
                          tags,
                          preconfigured_api_client
                          )
-        self.ucs_server_profile_name = ucs_server_profile_name
+        self.ucs_server_profile_names = ucs_server_profile_names
+        self.ucs_server_profile_template_names = ucs_server_profile_template_names
 
     def __repr__(self):
         return (
@@ -962,35 +1061,60 @@ class DirectlyAttachedUcsServerPolicy(UcsPolicy):
             f"'{self.intersight_base_url}', "
             f"{self.tags}, "
             f"{self.api_client}, "
-            f"'{self.ucs_server_profile_name}')"
+            f"'{self.ucs_server_profile_names}', "
+            f"'{self.ucs_server_profile_template_names}')"
             )
 
     def _attach_ucs_server_profile(self):
-        """This is a function to attach an Intersight UCS Server Profile to an
-        Intersight Policy.
+        """This is a function to attach Intersight UCS Server Profiles and/or
+        UCS Server Profile Templates to an Intersight Policy.
 
         Returns:
             A dictionary for the API body of the policy object to be posted on
             Intersight.    
         """
+        # Update the API body with the Profiles key
+        self.intersight_api_body["Profiles"] = []        
         # Attach UCS Server Profile
-        if self.ucs_server_profile_name:
-            print("Attaching the UCS Server Profile named "
-                  f"{self.ucs_server_profile_name}...")
-            # Get UCS Server Profile MOID
-            ucs_server_profile_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
-                                                                       intersight_api_key=None,
-                                                                       object_name=self.ucs_server_profile_name,
-                                                                       intersight_api_path="server/Profiles",
-                                                                       object_type="UCS Server Profile",
-                                                                       organization=self.organization,
-                                                                       preconfigured_api_client=self.api_client
-                                                                       )
-            # Update the API body with the appropriate Server Profile MOID
-            self.intersight_api_body["Profiles"] = [
-                {"Moid": ucs_server_profile_moid,
-                 "ObjectType": "server.Profile"}
-                ]
+        if self.ucs_server_profile_names:
+            provided_ucs_server_profile_list = string_to_list_maker(self.ucs_server_profile_names)
+            for provided_ucs_server_profile in provided_ucs_server_profile_list:
+                print("Attaching the UCS Server Profile named "
+                      f"{provided_ucs_server_profile}...")
+                # Get UCS Server Profile MOID
+                ucs_server_profile_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
+                                                                           intersight_api_key=None,
+                                                                           object_name=provided_ucs_server_profile,
+                                                                           intersight_api_path="server/Profiles?$top=1000",
+                                                                           object_type="UCS Server Profile",
+                                                                           organization=self.organization,
+                                                                           preconfigured_api_client=self.api_client
+                                                                           )
+                # Update the API body with the appropriate Server Profile MOID
+                self.intersight_api_body["Profiles"].append(
+                    {"Moid": ucs_server_profile_moid,
+                     "ObjectType": "server.Profile"}
+                    )
+        # Attach UCS Server Profile Templates
+        if self.ucs_server_profile_template_names:
+            provided_ucs_server_profile_template_list = string_to_list_maker(self.ucs_server_profile_template_names)
+            for provided_ucs_server_profile_template in provided_ucs_server_profile_template_list:
+                print("Attaching the UCS Server Profile Template named "
+                      f"{provided_ucs_server_profile_template}...")
+                # Get UCS Server Profile MOID
+                ucs_server_profile_template_moid = intersight_object_moid_retriever(intersight_api_key_id=None,
+                                                                                    intersight_api_key=None,
+                                                                                    object_name=provided_ucs_server_profile_template,
+                                                                                    intersight_api_path="server/ProfileTemplates?$top=1000",
+                                                                                    object_type="UCS Server Profile Template",
+                                                                                    organization=self.organization,
+                                                                                    preconfigured_api_client=self.api_client
+                                                                                    )
+                # Update the API body with the appropriate Server Profile MOID
+                self.intersight_api_body["Profiles"].append(
+                    {"Moid": ucs_server_profile_template_moid,
+                     "ObjectType": "server.ProfileTemplate"}
+                    )
 
     def object_maker(self):
         """This function makes the targeted policy object.
@@ -1024,7 +1148,8 @@ class SerialOverLanPolicy(DirectlyAttachedUcsServerPolicy):
                  intersight_base_url="https://www.intersight.com/api/v1",
                  tags=None,
                  preconfigured_api_client=None,
-                 ucs_server_profile_name="",
+                 ucs_server_profile_names="",
+                 ucs_server_profile_template_names="",
                  enable_serial_over_lan=True,
                  com_port="com0",
                  baud_rate=9600,
@@ -1038,7 +1163,8 @@ class SerialOverLanPolicy(DirectlyAttachedUcsServerPolicy):
                          intersight_base_url,
                          tags,
                          preconfigured_api_client,
-                         ucs_server_profile_name
+                         ucs_server_profile_names,
+                         ucs_server_profile_template_names
                          )
         self.enable_serial_over_lan = enable_serial_over_lan
         self.com_port = com_port
@@ -1064,7 +1190,8 @@ class SerialOverLanPolicy(DirectlyAttachedUcsServerPolicy):
             f"'{self.intersight_base_url}', "
             f"{self.tags}, "
             f"{self.api_client}, "
-            f"'{self.ucs_server_profile_name}', "
+            f"'{self.ucs_server_profile_names}', "
+            f"'{self.ucs_server_profile_template_names}', "
             f"{self.enable_serial_over_lan}, "
             f"{self.com_port}, "
             f"{self.baud_rate}, "
@@ -1072,7 +1199,6 @@ class SerialOverLanPolicy(DirectlyAttachedUcsServerPolicy):
             )
 
 
-# Establish function to make Policy
 def serial_over_lan_policy_maker(
     intersight_api_key_id,
     intersight_api_key,
@@ -1086,7 +1212,8 @@ def serial_over_lan_policy_maker(
     intersight_base_url="https://www.intersight.com/api/v1",
     tags=None,
     preconfigured_api_client=None,
-    ucs_server_profile_name=""
+    ucs_server_profile_names="",
+    ucs_server_profile_template_names=""
     ):
     """This is a function used to make a Serial Over LAN Policy on Cisco Intersight.
 
@@ -1130,9 +1257,16 @@ def serial_over_lan_policy_maker(
             is provided, empty strings ("") or None can be provided for the
             intersight_api_key_id, intersight_api_key, and intersight_base_url
             arguments.
-        ucs_server_profile_name (str):
-            Optional; The UCS Server Profile the policy should be attached to.
-            The default value is an empty string ("").
+        ucs_server_profile_names (str):
+            Optional; The UCS Server Profiles the policy should be attached
+            to. If providing more than one UCS Server Profile, additional 
+            entries should be comma-separated. The default value is an empty
+            string ("").
+        ucs_server_profile_template_names (str):
+            Optional; The UCS Server Profile Templates the policy should be
+            attached to. If providing more than one UCS Server Profile Template,
+            additional entries should be comma-separated. The default value is
+            an empty string ("").
     """
     def builder(target_object):
         """This is a function used to build the objects that are components of
@@ -1169,7 +1303,8 @@ def serial_over_lan_policy_maker(
             intersight_base_url=intersight_base_url,
             tags=tags,
             preconfigured_api_client=preconfigured_api_client,
-            ucs_server_profile_name=ucs_server_profile_name,
+            ucs_server_profile_names=ucs_server_profile_names,
+            ucs_server_profile_template_names=ucs_server_profile_template_names,
             enable_serial_over_lan=enable_serial_over_lan,
             com_port=com_port,
             baud_rate=baud_rate,
@@ -1213,7 +1348,8 @@ def main():
         intersight_base_url=intersight_base_url,
         tags=serial_over_lan_policy_tags,
         preconfigured_api_client=main_intersight_api_client,
-        ucs_server_profile_name=ucs_server_profile_name
+        ucs_server_profile_names=ucs_server_profile_names,
+        ucs_server_profile_template_names=ucs_server_profile_template_names
         )
 
     # Policy Maker completion
